c0c8b5860eb6d956dfaa13fd9addc1706982f06a
Restu Hikmal <restuhikmal07@gmail.com>
2023-03-09 15:12:55 +0700

    masih part 3

--- Assets/Pro Longbow Pack/Scripts/ArrowShoot.cs
+++ Assets/Pro Longbow Pack/Scripts/ArrowShoot.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowShoot : MonoBehaviour
@@ -13,7 +11,7 @@ public class ArrowShoot : MonoBehaviour
     {
        Vector2 ScreenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
        Ray ray = Camera.main.ScreenPointToRay(ScreenCenter);
-       if(Physics.Raycast(ray, out hit, range));
+       if(Physics.Raycast(ray, out hit, range))
        {
             GameObject ArrowInstantiate = GameObject.Instantiate(ArrowPrefab, ArrowSpawnPosition.transform.position, ArrowSpawnPosition.transform.rotation) as GameObject;
             ArrowInstantiate.GetComponent<Arrow>().setTarget(hit.point);
