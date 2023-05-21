564b0f33586bffd892f4c451b8f1b1c91a440efd
charlesbintang <charlesbintang203.com>
2023-05-22 00:48:25 +0700

    Billboard active

--- Assets/Aset Musuh/Script/Billboard.cs
+++ Assets/Aset Musuh/Script/Billboard.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    [SerializeField]
-    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        GameObject target = GameObject.Find("CameraRig");
+        transform.LookAt(target.transform);
     }
 }
